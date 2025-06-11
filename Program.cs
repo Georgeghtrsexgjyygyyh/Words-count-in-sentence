08c7aa01c3aa5519d394ba91d37370e11f6cafaf
George <95586665+Georgeghtrsexgjyygyyh@users.noreply.github.com>
2025-06-11 14:56:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Program_word_count {
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        //script
+
+            Console.Title = "Words couting";
+
+
+            Console.WriteLine("Enter a sentence: ");
+
+            string? sentence = Console.ReadLine();
+
+            int sentence_length = CoutingLenght(sentence);
+
+            Console.WriteLine("There are " + sentence_length + " words in that sentence.");
+
+
+
+            //Wait before end
+            Console.ReadKey();
+
+        //script
+        }
+
+
+        static int CoutingLenght(string sentence)
+        {
+            int sentence_length = Convert.ToInt32(sentence.Split(' ').Length);
+            return sentence_length;
+        }
+
+    }
+
+}
